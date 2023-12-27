d524cccdfd5936f0b2e568d0673a21ea53ea95d3
Simon Solnes <simon@hyre.no>
2023-12-27 15:37:04 +0100

    Sourcekit

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -20,8 +20,28 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 -- Swift
-require('lspconfig').sourcekit.setup({
-	cmd = { '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp' }
+-- require('lspconfig').sourcekit.setup({
+-- 	cmd = { '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp' },
+-- 	--cmd = { "sourcekit-lsp" },
+-- 	filetypes = { "swift", "c", "cpp", "objective-c", "objective-cpp" },
+-- 	root_dir = ".",
+-- })
+local swift_lsp = vim.api.nvim_create_augroup("swift_lsp", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "swift" },
+	callback = function()
+		local root_dir = vim.fs.dirname(vim.fs.find({
+			"Package.swift",
+			".git",
+		}, { upward = true })[1])
+		local client = vim.lsp.start({
+			name = "sourcekit-lsp",
+			cmd = { "sourcekit-lsp" },
+			root_dir = root_dir,
+		})
+		vim.lsp.buf_attach_client(0, client)
+	end,
+	group = swift_lsp,
 })
 
 -- Lua
