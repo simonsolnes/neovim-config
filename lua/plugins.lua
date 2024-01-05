bce3278d222b352b438775eedba081d5edecaee7
Simon Solnes <simon@hyre.no>
2024-01-05 21:13:51 +0100

    Add folke/trouble

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -315,5 +315,14 @@ require('lazy').setup({
 	},
 	{
 		'https://github.com/simrat39/rust-tools.nvim',
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
 	}
 }, {})
